bfb60506a0217d70f9fa5ec04a46abf443686078
omoikanelulu <r.omoikane@gmail.com>
2022-07-08 11:49:41 +0900

    プッシュのテスト

---
+++ src/html/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- リセットcssの読み込み -->
+    <link rel="stylesheet" href="./css/reset.css">
+    <!-- 自作cssの読み込み -->
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Three_good_thingsへようこそ</title>
+</head>
+
+<body>
+    <header>
+    </header>
+    <main>
+        <p>Hallo world</p>
+    </main>
+    <footer>
+    </footer>
+</body>
+
+</html>
